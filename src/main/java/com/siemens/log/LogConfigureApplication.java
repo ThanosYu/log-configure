157becc288f0447d1f20152cf9276e351fc0235a
Z003ZDBC <shiqiang.yu@siemens.com>
2018-09-29 15:09:50 +0800

    1

---
+++ src/main/java/com/siemens/log/LogConfigureApplication.java
@@ -0,0 +1,12 @@
+package com.siemens.log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogConfigureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogConfigureApplication.class, args);
+	}
+}
---
+++ src/main/java/com/siemens/log/service/LogService.java
@@ -0,0 +1,10 @@
+package com.siemens.log.service;
+
+/**
+ * @author Thanos Yu
+ * @date 9/28/2018 2:02 PM
+ */
+public interface LogService {
+
+    void pringLog();
+}
