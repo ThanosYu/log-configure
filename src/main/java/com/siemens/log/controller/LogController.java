a5518367829cc1a0a61d74fc6459c91757ce00ca
Z003ZDBC <shiqiang.yu@siemens.com>
2018-10-09 17:51:55 +0800

    config ip for log

--- src/main/java/com/siemens/log/controller/LogController.java
+++ src/main/java/com/siemens/log/controller/LogController.java
@@ -1,10 +1,8 @@
 package com.siemens.log.controller;
 
 import ch.qos.logback.classic.LoggerContext;
-import com.siemens.log.service.LogService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,30 +15,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/v1/logConfigure")
 public class LogController {
-    @Autowired
-    private LogService logService;
 
     private Logger log = LoggerFactory.getLogger(LogController.class);
 
-    @RequestMapping(value = "/level",method=RequestMethod.GET)
+    @RequestMapping(value = "/level", method = RequestMethod.GET)
     public String updateLogbackLevel(@RequestParam(value = "level") String level,
                                      @RequestParam(value = "packageName", defaultValue = "") String packageName) throws Exception {
-        System.out.println("--------------------------------------level: " + level);
-        System.out.println("--------------------------------------packageName: " + packageName);
-
-        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
-        if (packageName == null || packageName.isEmpty()) {
-            System.out.println("--------------------all");
-            loggerContext.getLogger("root").setLevel(ch.qos.logback.classic.Level.toLevel(level));
-        } else {
-            loggerContext.getLogger(packageName).setLevel(ch.qos.logback.classic.Level.valueOf(level));
-            System.out.println("--------------------part");
+        try {
+            LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+            if (packageName == null || packageName.isEmpty()) {
+                loggerContext.getLogger("root").setLevel(ch.qos.logback.classic.Level.toLevel(level));
+                log.debug("--------------------------------------change log level of all ");
+                log.info("--------------------------------------change log level of all ");
+            } else {
+                loggerContext.getLogger(packageName).setLevel(ch.qos.logback.classic.Level.valueOf(level));
+                log.debug("--------------------------------------change log level of " + packageName);
+                log.info("--------------------------------------change log level of " + packageName);
+            }
+        } catch (Exception e) {
+            log.info("--------------------------------------Change log level failed");
         }
-
-        log.debug("--------------------------------------debug: ");
-        log.info("--------------------------------------info: ");
-        log.error("--------------------------------------error: ");
-        logService.pringLog();
         return "success";
     }
 }
---
+++ src/main/java/com/siemens/log/tools/IPLogConfig.java
@@ -0,0 +1,24 @@
+package com.siemens.log.tools;
+
+import ch.qos.logback.classic.pattern.ClassicConverter;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * @author Thanos Yu
+ * @date 10/9/2018 5:32 PM
+ */
+public class IPLogConfig extends ClassicConverter {
+
+    @Override
+    public String convert(ILoggingEvent event) {
+        try {
+            return InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
